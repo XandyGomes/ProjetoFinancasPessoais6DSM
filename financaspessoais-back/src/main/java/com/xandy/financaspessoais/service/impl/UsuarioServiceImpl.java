b064faf5fbe8843ea8958f4c69eba25049e9e653
XandyGomes <x3.computadores@hotmail.com>
2025-03-15 09:45:56 -0300

    Aula.15.03.2025

--- financaspessoais-back/src/main/java/com/xandy/financaspessoais/service/impl/UsuarioServiceImpl.java
+++ financaspessoais-back/src/main/java/com/xandy/financaspessoais/service/impl/UsuarioServiceImpl.java
@@ -1,8 +1,12 @@
 package com.xandy.financaspessoais.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.xandy.financaspessoais.exception.ErroAutenticacao;
 import com.xandy.financaspessoais.exception.RegraNegocioException;
 import com.xandy.financaspessoais.model.entity.Usuario;
 import com.xandy.financaspessoais.model.repository.UsuarioRepository;
@@ -12,7 +16,7 @@ import com.xandy.financaspessoais.service.UsuarioService;
 public class UsuarioServiceImpl implements UsuarioService {
 
 	private UsuarioRepository repository;
-	
+
 	@Autowired
 	public UsuarioServiceImpl(UsuarioRepository repository) {
 		super();
@@ -21,20 +25,31 @@ public class UsuarioServiceImpl implements UsuarioService {
 
 	@Override
 	public Usuario autenticar(String email, String senha) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Optional<Usuario> usuario = repository.findByEmail(email);
+
+		if (!usuario.isPresent()) {
+			throw new ErroAutenticacao("usuário não encontrado para o e-mail informado.");
+		}
+
+		if (!usuario.get().getSenha().equals(senha)) {
+			throw new ErroAutenticacao("Senha inválida.");
+		}
+
+		return usuario.get();
 	}
 
 	@Override
+	@Transactional
 	public Usuario salvarUsuario(Usuario usuario) {
-		// TODO Auto-generated method stub
-		return null;
+		validarEmail(usuario.getEmail());
+		return repository.save(usuario);
 	}
 
 	@Override
 	public void validarEmail(String email) {
 		boolean existe = repository.existsByEmail(email);
-		if(existe) {
+		if (existe) {
 			throw new RegraNegocioException("Já existe um usuário cadastrado com este e-mail");
 		}
 	}
---
+++ financaspessoais-back/src/test/java/com/xandy/financaspessoais/model/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.xandy.financaspessoais.model.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class UsuarioRepositoryTest {
+	
+	@Autowired
+	UsuarioRepository repository;
+	
+	@Test
+	public void deveVerificarAExistenciaDeUmEmail() {
+		//cenário
+		
+		//ação/execução
+		
+		//verificação
+		
+	}
+}
