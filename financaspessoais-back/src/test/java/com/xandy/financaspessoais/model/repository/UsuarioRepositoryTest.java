7e5d8c4d53fb46978c49a7ebaa455a6073dab8c8
XandyGomes <x3.computadores@hotmail.com>
2025-03-15 10:27:42 -0300

    Aula.15.03.2025

--- financaspessoais-back/src/test/java/com/xandy/financaspessoais/model/repository/UsuarioRepositoryTest.java
+++ financaspessoais-back/src/test/java/com/xandy/financaspessoais/model/repository/UsuarioRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.xandy.financaspessoais.model.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.xandy.financaspessoais.model.entity.Usuario;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 	
 	@Autowired
@@ -18,10 +21,19 @@ public class UsuarioRepositoryTest {
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
 		//cenário
+		Usuario usuario = Usuario
+							.builder()
+							.nome("Alexandre")
+							.email("alexandre@email.com")
+							.senha("1234")
+							.build();
+		repository.save(usuario);
 		
 		//ação/execução
+		boolean result = repository.existsByEmail("alexandre@email.com");
 		
 		//verificação
+		Assertions.assertThat(result).isTrue();
 		
 	}
 }
