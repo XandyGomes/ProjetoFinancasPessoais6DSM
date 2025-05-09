388a1be73856827f356bba7bff361d8e3900ac80
XandyGomes <x3.computadores@hotmail.com>
2025-05-09 20:33:25 -0300

    Aula.09.05.2025

---
+++ financaspessoais-front/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "bootswatch/dist/flatly/bootstrap.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>VAMOS COMEÇAR O FRONT!!!</h1>
+        <button type="button" className="btn btn-success">
+          Success
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
