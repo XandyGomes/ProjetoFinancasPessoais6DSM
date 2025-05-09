88c0f9d8e6686e1acc24b007a6990bacf2451589
XandyGomes <x3.computadores@hotmail.com>
2025-05-09 19:34:37 -0300

    Aula.09.05.2025

--- financaspessoais-back/src/main/java/com/xandy/financaspessoais/model/repository/LancamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xandy.financaspessoais.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.xandy.financaspessoais.model.entity.Lancamento;
-
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
-
-}
